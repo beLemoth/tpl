23718f3c40453e3d68d8f1ac2ee9dcb4b227241f
beLemoth <ilin.job@gmail.com>
2017-11-30 00:01:49 +0300

    fix bom task3

--- bom/3/assets/js/main.js
+++ bom/3/assets/js/main.js
@@ -1,13 +1,15 @@
 'use strict';
 
-let button = document.getElementById('addBlock'),
+let addButton = document.getElementById('addBlock'),
+    saveButton = document.getElementById('saveBlocks'),
+    clearButton = document.getElementById('clear'),
     target = document.getElementById('target');
 
 let droping,
     offsetY,
     offsetX;
 
-function randomColor() {
+let randomColor = () => {
 
     let chars = '0123456789ABCDEF';
     let color = '#';
@@ -17,45 +19,89 @@ function randomColor() {
     }
 
     return color;
-}
+};
 
-function randomValue(min,max) {
+let randomValue = (min,max) => {
     return Math.random() * (max - min) + min;
-}
+};
 
-let insertBlock = () => {
+let initBlock = blockParams => {
 
-    let block = document.createElement('div'),
-        height = randomValue(0,600),
-        width = randomValue(0,1000);
+    let params = blockParams.split('&'),
+        block = document.createElement('div');
 
+    block.style.top = params[0];
+    block.style.left = params[1];
+    block.style.width = params[2];
+    block.style.height = params[3];
+    block.style.backgroundColor = params[4];
 
-    block.style.backgroundColor = '' + randomColor();
-    block.style.width = width + 'px';
-    block.style.height = height + 'px';
-    block.style.top = randomValue(0,window.innerHeight - height) + 'px';
-    block.style.left = randomValue(0,window.innerWidth - width) + 'px';
     block.style.border = 'solid 1px #000000';
     block.classList.add('newBlock');
 
     target.appendChild(block);
-
-    saveDivs();
-
 };
 
-button.addEventListener('click',insertBlock);
+let insertBlock = () => {
+
+    let height = randomValue(0,600),
+        width = randomValue(0,1000),
+        top = randomValue(0,window.innerHeight - height),
+        left = randomValue(0,window.innerWidth - width),
+        color = '' + randomColor(),
+        params = `${top}px&${left}px&${width}px&${height}px&${color}`;
+
+    initBlock(params);
+};
+
+
+let saveDivs = () => {
+    let divs = document.getElementsByClassName('newBlock');
+
+    [].forEach.call(divs, (elem,idx) => {
+        let date = new Date();
+        date.setTime(date.getTime()+10000000000);
+        document.cookie = `block${idx+1}=${elem.style.top}&${elem.style.left}&${elem.style.width}&${elem.style.height}&${elem.style.backgroundColor}; expires=${date.toUTCString()}`;
+    });
+};
+
+let loadBlocks = () => {
+    let cookies = document.cookie.split('; ');
+
+    cookies.forEach(cookieString => {
+        let cookieArray = cookieString.split('=');
+
+        initBlock(cookieArray[1]);
+    });
+};
+
+let clearCookies = () => {
+    let cookies = document.cookie.split('; ');
+
+    cookies.forEach(cookieString => {
+        let cookieArray = cookieString.split('=');
+
+        let date = new Date();
+        date.setTime(date.getTime()-1);
+
+        document.cookie = `${cookieArray[0]}=0; expires=${date.toUTCString()}`;
+    });
+};
+
+// EVENTS
+
+addButton.addEventListener('click',insertBlock);
+saveButton.addEventListener('click',saveDivs);
+clearButton.addEventListener('click',clearCookies);
 
 target.addEventListener('mousedown',(e) => {
     droping = true;
-
-    console.dir(e);
 
     offsetY = e.offsetY;
     offsetX = e.offsetX;
 });
 
-target.addEventListener('mouseup',() => {
+target.addEventListener('mouseup',() => {           // reset droping
     droping = false;
 });
 
@@ -66,19 +112,10 @@ target.addEventListener('mousemove',(e) => {
         e.target.style.top = e.clientY - offsetY + 'px';
         e.target.style.left = e.clientX - offsetX + 'px';
     }
-
 });
 
-let saveDivs = () => {
-    let divs = document.getElementsByClassName('newBlock');
+// ON PAGE LOAD
 
-    [].forEach.call(divs, elem => {
-        let date = new Date();
-        date.setTime(date.getTime()+10000000000);
-
-        document.cookie = `${elem.style.top},${elem.style.left},${elem.style.width},${elem.style.height},${elem.style.backgroundColor}; expires=${date.toUTCString()}`;
-
-    });
-};
+if(document.cookie) loadBlocks();       // if there is a saved blocks
 
 
